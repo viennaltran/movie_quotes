c6da1e3ec363a7d205cc47303ea15a470798a8f7
viennaltran <lanvienhtran@gmail.com>
2018-11-28 17:04:43 -0800

    Completed fake sign in sign out functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,21 @@
 import types from './types';
 import axios from 'axios';
+
+export function userSignIn(){
+    // console.log('User Sign In Called');
+    return{
+        type:types.SIGN_IN
+    }
+}
+
+export function userSignOut(){
+    return{
+        type:types.SIGN_OUT
+    }
+}
+
+export function userSignUp(){
+    return{
+        type:types.SIGN_UP
+    }
+}
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { userSignIn, userSignOut} from '../actions';
 
 class Nav extends Component {
+    renderLinks(){
+        const { auth, signIn, signOut} = this.props;
+
+        if(auth) {
+            return <button onClick={signOut} className="btn yellow darken-2">Sign Out</button>
+        }
+
+        return <button onClick={signIn} className="btn red darken-2">Sign In</button>
+    }
+
     render(){
         const navStyle ={
             padding:'0 8px'
         }
+
+        // console.log('User Auth:', this.props.auth);
+
         return(
             <nav style={navStyle} className="orange darken-5">
                 <div className="nav-wrapper">
@@ -28,7 +43,7 @@ class Nav extends Component {
                         <Link to="/quotes">Quotes</Link>
                     </li>
                     <li>
-                        <Link to="/sign-in">Sign In</Link>
+                        {this.renderLinks()}
                     </li>
                     <li>
                         <Link to="/sign-up">Sign Up</Link>
@@ -41,4 +56,13 @@ class Nav extends Component {
     }
 }
 
-export default Nav;
+function mapStateToProps(state){
+    return {
+        auth: state.user.auth 
+    }
+}
+
+export default connect(mapStateToProps, {
+    signIn:userSignIn,
+    signOut:userSignOut
+}) (Nav);
