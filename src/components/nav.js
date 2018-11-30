cd1c2d0949e9b10307639e08d73d7939200af00d
viennaltran <lanvienhtran@gmail.com>
2018-11-30 12:08:47 -0800

    Updated nav functionality to display links dependent on user auth status

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,39 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import {Link} from 'react-router-dom';
 import { connect } from 'react-redux';
-import { userSignIn, userSignOut} from '../actions';
+import {userSignOut} from '../actions';
 
 class Nav extends Component {
+    
     renderLinks(){
-        const { auth, signIn, signOut} = this.props;
+        const { auth, signOut} = this.props;
 
         if(auth) {
-            return <button onClick={signOut} className="btn yellow darken-2">Sign Out</button>
+            return (
+                <Fragment>
+                <li>
+                    <Link to="/secret-list">Secret List</Link>
+                </li>
+                <li>
+                    <Link to="/quotes">Quotes</Link>
+                </li>
+                <li>
+                    <button onClick={signOut} className="btn yellow darken-2">Sign Out</button>
+                </li>
+                </Fragment>
+            );
         }
 
-        return <button onClick={signIn} className="btn red darken-2">Sign In</button>
+            return (
+                <Fragment>
+                    <li>
+                        <Link to="/sign-in">Sign In</Link>
+                    </li>
+                    <li>
+                        <Link to="/sign-up">Sign Up</Link>
+                    </li>
+                </Fragment>
+            );
     }
 
     render(){
@@ -36,18 +58,8 @@ class Nav extends Component {
                     <li>
                         <Link to="/public-list">Public List</Link>
                     </li>
-                    <li>
-                        <Link to="/secret-list">Secret List</Link>
-                    </li>
-                    <li>
-                        <Link to="/quotes">Quotes</Link>
-                    </li>
-                    <li>
-                        {this.renderLinks()}
-                    </li>
-                    <li>
-                        <Link to="/sign-up">Sign Up</Link>
-                    </li>
+                    
+                    {this.renderLinks()}
                 </ul>
                 </div>
             </nav>
@@ -63,6 +75,5 @@ function mapStateToProps(state){
 }
 
 export default connect(mapStateToProps, {
-    signIn:userSignIn,
     signOut:userSignOut
 }) (Nav);
